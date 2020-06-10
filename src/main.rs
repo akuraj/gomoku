59abe62fbad9d540a9bc2641367a9413be7f04c0
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-06-10 18:12:40 +0800

    profile a search

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,15 @@ use consts::BLACK;
 use state::get_state;
 use testing::test_search_fns;
 use std::time::Instant;
+use threat_space_search::tss_board;
 
 fn main() {
-    let s = get_state(&["a1", "a2", "a3", "a13", "a14", "a15", "b1", "b15", "c1", "c15",
-                        "f14", "g13", "i9", "i10", "m1", "m15", "n1", "n15", "o1", "o2",
-                        "o3", "o13", "o14", "o15"],
-                      &["i6", "i13", "j10"],
-                      BLACK,
-                      false);
+    // let s = get_state(&["a1", "a2", "a3", "a13", "a14", "a15", "b1", "b15", "c1", "c15",
+    //                     "f14", "g13", "i9", "i10", "m1", "m15", "n1", "n15", "o1", "o2",
+    //                     "o3", "o13", "o14", "o15"],
+    //                   &["i6", "i13", "j10"],
+    //                   BLACK,
+    //                   false);
 
     // let s = get_state(&["f5", "g5", "h5", "g6", "g7", "h7", "i7", "h8", "h9", "g9", "i9"],
     //                   &["g4", "e5", "f6", "h6", "j6", "f7", "j7", "f8", "g8", "i8", "f9"],
@@ -40,15 +41,20 @@ fn main() {
     //                   BLACK,
     //                   true);
 
-    // let s = get_state(&["j5", "j6", "i10", "i11"],
-    //                   &[],
-    //                   BLACK,
-    //                   false );
+    let mut s = get_state(&["j5", "j6", "i10", "i11"],
+                          &[],
+                          BLACK,
+                          false );
 
-    println!("{}", s);
+    let n = 10;
 
-    // test_search_fns();
-    // let start = Instant::now();
-    // test_search_fns();
-    // println!("Time taken: {} seconds", ( start.elapsed().as_nanos() as f32 ) / 1e9);
+    for _ in 0..n {
+        tss_board(&mut s.board, s.turn);
+    }
+
+    let start = Instant::now();
+    for _ in 0..n {
+        tss_board(&mut s.board, s.turn);
+    }
+    println!("Time taken: {} seconds", ( start.elapsed().as_nanos() as f32 ) / 1e9);
 }
