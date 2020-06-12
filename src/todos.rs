1001787cc25fd5faecef0a79b7b6c2f265451e70
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-06-12 22:25:28 +0800

    some todos

--- src/todos.rs
+++ src/todos.rs
@@ -1,11 +1,13 @@
 //! A place for general FIXMEs and TODOs.
 
+// FIXME: Add all the comments in Python code to the Rust code.
+// FIXME: Get the todos from the Python code and put them here.
+// FIXME: Find better place to put these FIXMEs.
 // FIXME: usize is the appropriate type for WIN_LENGTH, MAX_DEFCON, MDFIT?
 // FIXME: isize, usize and other stuff inside geometry fns?
 //        Use more appropriate data types that take up less space like i8 or u8?
 // FIXME: Replace if-else-if with match!
 // FIXME: Add function documentation. Just copy from Python impl.
-// FIXME: Add all the comments in Python code to the Rust code.
 
 // TODO: Add checks on size of board and patterns by having max value in consts.
 // TODO: Just use i8 for all indices and sizes?
