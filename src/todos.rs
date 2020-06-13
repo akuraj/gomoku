f84ef7bf6acd1100eb1c18c11925652371d08865
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-06-13 12:25:58 +0800

    add todos

--- src/todos.rs
+++ src/todos.rs
@@ -1,8 +1,20 @@
 //! A place for general FIXMEs and TODOs.
 
 // FIXME: Add all the comments in Python code to the Rust code.
-// FIXME: Get the todos from the Python code and put them here.
-// FIXME: Find better place to put these FIXMEs.
+//        1. consts
+//        2. board
+//        3. geometry
+//        4. pattern_search
+//        5. pattern
+//        6. state
+//        7. testing
+//        8. threat_space_search
+//        9. todos
+// FIXME: get the todos from the Python code and put them here.
+// FIXME: Find better place to put these FIXMEs and TODOs.
+// FIXME: Use test feature and setup travis.
+// FIXME: usize i32/isize anywhere where we store them, instead of usize?
+// FIXME: Chcek on integer overflow checking in rust.
 // FIXME: usize is the appropriate type for WIN_LENGTH, MAX_DEFCON, MDFIT?
 // FIXME: isize, usize and other stuff inside geometry fns?
 //        Use more appropriate data types that take up less space like i8 or u8?
