ee098075f50421bccc4db66984540092b2906d81
Jaya Kiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-06-14 23:55:43 +0800

    add documentation to the geometry module.

--- src/geometry.rs
+++ src/geometry.rs
@@ -4,8 +4,10 @@ use num::{abs, signum};
 use std::cmp::{max, min};
 use std::collections::HashSet;
 
+/// Represents a point on the board.
 pub type Point = (isize, isize);
 
+/// Row increment when moving in direction 'd'.
 pub fn increment(d: isize) -> isize {
     if d % 4 == 0 {
         0
@@ -16,10 +18,12 @@ pub fn increment(d: isize) -> isize {
     }
 }
 
+/// Row and Column increments when moving in direction 'd'.
 pub fn increments(d: isize) -> (isize, isize) {
     (increment(d), increment(d + 2))
 }
 
+/// Possible starting indices of the pattern.
 pub fn index_bounds(side: isize, length: isize, increment: isize) -> (isize, isize) {
     if length <= side {
         match increment {
@@ -33,6 +37,7 @@ pub fn index_bounds(side: isize, length: isize, increment: isize) -> (isize, isi
     }
 }
 
+/// Possible relative starting indices of the pattern.
 pub fn index_bounds_incl(
     side: isize,
     length: isize,
@@ -76,6 +81,7 @@ pub fn index_bounds_incl(
     (-min(back, length - 1), min(front, length) - (length - 1))
 }
 
+/// Check if the point is on the given line/line segment.
 pub fn point_is_on_line(point: Point, start: Point, end: Point, segment_only: bool) -> bool {
     let dx1 = point.0 - start.0;
     let dy1 = point.1 - start.1;
@@ -84,6 +90,7 @@ pub fn point_is_on_line(point: Point, start: Point, end: Point, segment_only: bo
     (dx1 * dy2 == dx2 * dy1) && (!segment_only || (dx1 * dx2 <= 0 && dy1 * dy2 <= 0))
 }
 
+/// For lines along one of the 8 directions, returns the i'th point from start.
 pub fn point_on_line(start: Point, end: Point, i: isize) -> Point {
     let dx = end.0 - start.0;
     let dy = end.1 - start.1;
@@ -91,18 +98,23 @@ pub fn point_on_line(start: Point, end: Point, i: isize) -> Point {
     (start.0 + signum(dx) * i, start.1 + signum(dy) * i)
 }
 
+/// Check if the two points represent a normal line.
+/// A line along one of the 8 directions is a normal line.
 pub fn is_normal_line(start: Point, end: Point) -> bool {
     let adx = abs(end.0 - start.0);
     let ady = abs(end.1 - start.1);
     (adx * ady == 0 || adx == ady) && (adx + ady > 0)
 }
 
+/// Chebyshev distance between the two points.
 pub fn chebyshev_distance(start: Point, end: Point) -> isize {
     let adx = abs(end.0 - start.0);
     let ady = abs(end.1 - start.1);
     max(adx, ady)
 }
 
+/// Set of points, as specified by idxs, on the line spanning start and end.
+/// See point_on_line for more info.
 pub fn point_set_on_line(start: Point, end: Point, idxs: &[isize]) -> HashSet<Point> {
     let mut point_set: HashSet<Point> = HashSet::new();
 
@@ -113,6 +125,14 @@ pub fn point_set_on_line(start: Point, end: Point, idxs: &[isize]) -> HashSet<Po
     point_set
 }
 
+/// Modified slope intercept form.
+///
+/// Returns (a, b, c), where,
+///
+/// a * y = b * x + c
+///
+/// Normalized such that a is 1 if line is not vertical,
+/// and in the case of a vertical line, (a, b) = (0, 1).
 pub fn slope_intercept(start: Point, end: Point) -> (isize, isize, isize) {
     assert!(is_normal_line(start, end));
 
@@ -127,6 +147,12 @@ pub fn slope_intercept(start: Point, end: Point) -> (isize, isize, isize) {
     }
 }
 
+/// Unique identifier for a point on a given line.
+///
+/// (x, y) = point
+///
+/// Check the that point is on the given line,
+/// and return x if line is not vertical, and y otherwise.
 pub fn point_idx_on_line(point: Point, line: (isize, isize, isize)) -> isize {
     let (x, y) = point;
     assert!(line.0 * y == line.1 * x + line.2);
