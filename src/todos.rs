ef08f7c39606e7380d4160d6b821d8200fbc7ee8
Jaya Kiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-06-28 22:56:21 +0800

    update todos

--- src/todos.rs
+++ src/todos.rs
@@ -7,23 +7,20 @@
 //          Maintain, update, pass-down threats (both own and opp).
 //          Is this overall a good idea?
 //       1) Can play LowPri threat as long as the move also generates a HighPri threat!
-//       2) Search LowPri threats only if no win found via HighPri threats? Or always?
-//       3) Only search LowPri threats a max number of ply in a given variation!
+//       2) Only search LowPri threats a max number of ply in a given variation!
 //          We can control this via a LowPriMaxDepth(?) variable.
-//       4) Stop searching children if a win is found? Or find all possible wins?
-//       5) Stop searching and return False if opponent is winning via critical squares?
+//       3) Stop searching and return False if opponent is winning via critical squares?
 //          Under what conditions do we assume that opponent is winning?
 //          Make a note in search node when we cut off variation due to opponent win!
-//       6) Only store winning variations to help save space!
-//       7) Sort win variations by length? Shorter is better.
-//       8) Implement a transposition table. How do we cope with having different last_sqs?
+//       4) Only store winning variations to help save space!
+//       5) Sort win variations by length? Shorter is better.
+//       6) Implement a transposition table. How do we cope with having different last_sqs?
 //          A Zobrit hash? Or just use a dict? What's the most efficient data structure?
 //          A hash that depends on position as well as latest move?
-//       9) What's the correct/viable TSS impl according to the paper? How does mine compare?
-//       10) Max-depth for certain/all kinds of TSS?
-//       11) Threats data and other data should be lean to reduce memory usage.
-//       12) We don't actually need critical sqs for NON_IMMEDIATE threats.
-//           Should we keep them as they are currently?
+//       7) Max-depth for certain/all kinds of TSS?
+//       8) Threats data and other data should be lean to reduce memory usage.
+//       9) We don't actually need critical sqs for NON_IMMEDIATE threats.
+//          Should we keep them as they are currently?
 // TODO: Use Yixin to check for best moves in test positions.
 //       Use it to guide the development effort and debugging.
 // TODO: Check all test positions in paper. Implement regtests based on them.
